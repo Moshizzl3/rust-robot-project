51bd32a948cb574364a8876b1d26efadaf33989a
moshizzl3 <mohamedibr@gmail.com>
2025-11-01 23:55:20 +0100

    line sensor working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rppal::gpio::Gpio;
+use rppal::gpio::{Gpio, Level};
 use std::error::Error;
 use std::thread;
 use std::time::Duration;
@@ -6,29 +6,50 @@ mod motor;
 mod robot;
 use motor::Motor;
 use robot::Robot;
-fn main() -> Result<(), Box<dyn Error>> {
+// fn main() -> Result<(), Box<dyn Error>> {
+//     let gpio = Gpio::new()?;
+//     let mut robot = Robot::new(&gpio, "Rusty".to_string())?;
+
+//     robot.forward();
+//     thread::sleep(Duration::from_secs(2));
+
+//     robot.spin_left();
+//     thread::sleep(Duration::from_secs(1));
+
+//     robot.spin_right();
+//     thread::sleep(Duration::from_secs(1));
+
+//     robot.backward();
+//     thread::sleep(Duration::from_secs(2));
+
+//     robot.turn_right();
+//     thread::sleep(Duration::from_secs(2));
+
+//     robot.turn_left();
+//     thread::sleep(Duration::from_secs(2));
+
+//     robot.stop();
+
+//     Ok(())
+// }
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Line sensor test - GPIO 25");
     let gpio = Gpio::new()?;
-    let mut robot = Robot::new(&gpio, "Rusty".to_string())?;
 
-    robot.forward();
-    thread::sleep(Duration::from_secs(2));
+    // pin 25 as input from docs
+    let line_sensor = gpio.get(25)?.into_input_pullup();
 
-    robot.spin_left();
-    thread::sleep(Duration::from_secs(1));
+    println!("Move sensor over black and white surfaces...");
+    println!("Press Ctrl+C to exit\n");
 
-    robot.spin_right();
-    thread::sleep(Duration::from_secs(1));
+    loop {
+        let level = line_sensor.read();
 
-    robot.backward();
-    thread::sleep(Duration::from_secs(2));
-
-    robot.turn_right();
-    thread::sleep(Duration::from_secs(2));
-
-    robot.turn_left();
-    thread::sleep(Duration::from_secs(2));
-
-    robot.stop();
-
-    Ok(())
+        match level {
+            Level::High => println!("is high"),
+            Level::Low => println!("is low"),
+        }
+        thread::sleep(Duration::from_millis(500));
+    }
 }
