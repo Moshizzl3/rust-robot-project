878a6299614c8a9debac1be4e6bdfd60ae8589dd
moshizzl3 <mohamedibr@gmail.com>
2025-11-05 23:55:30 +0100

    playing with pwm

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,28 @@ mod robot;
 use motor::Motor;
 use robot::{LineSensor, Robot};
 
+// Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
+const GPIO_PWM: u8 = 7;
+
+// Servo configuration. Change these values based on your servo's verified safe
+// minimum and maximum values.
+//
+// Period: 20 ms (50 Hz). Pulse width: min. 1200 µs, neutral 1500 µs, max. 1800 µs.
+const PERIOD_MS: u64 = 20;
+const PULSE_MIN_US: u64 = 1200;
+const PULSE_NEUTRAL_US: u64 = 1500;
+const PULSE_MAX_US: u64 = 1800;
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let gpio = Gpio::new()?;
-    let mut robot = Robot::new(&gpio, "Rusty".to_string())?;
+    let mut pin = Gpio::new()?.get(GPIO_PWM)?.into_output();
 
-    println!("Starting line follower...m");
-    println!("Press Ctrl+C to stop");
+    // Enable software-based PWM with the specified period, and rotate the servo by
+    // setting the pulse width to its maximum value.
 
-    loop {
-        robot.follow_line_pulse(20);
-        thread::sleep(Duration::from_millis(50)); // How fast to check?
-    }
-    robot.stop();
+    // Rotate the servo to the opposite side.
+    pin.set_pwm_frequency(1000.0, 0.50)?;
+
+    thread::sleep(Duration::from_millis(1500));
 
     Ok(())
 }
